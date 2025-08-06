f34b5cf5f3260b0f5ac6231f59b3fa29d47cc198
abbasbayat0 <abbas0bayat@gmail.com>
2025-08-07 00:26:12 +0330

    type alias & intersection types

--- type-script/src/App.tsx
+++ type-script/src/App.tsx
@@ -1,15 +1,38 @@
 const App = () => {
-  const processData = (
-    input: string | number,
-    config: { reverse: boolean } = { reverse: false }
-  ): string | number | undefined => {
-    if (typeof input === "number") return input * input;
-    else if (typeof input === "string" && config.reverse)
-      return input.split("").reverse().join("");
-    else if (typeof input === "string") return input.toUpperCase();
+  type Employee = { id: number; name: string; department: string };
+  type Manager = { id: number; name: string; employees: Employee[] };
+  type Staff = Employee | Manager;
+
+  const PrintStafDetail = (input: Staff): void => {
+    if ("employees" in input) {
+      console.log(
+        `${input.name} is a manager with ${input.employees.length} of employees`
+      );
+    } else {
+      console.log(
+        `${input.name} is an employee in ${input.department} department`
+      );
+    }
   };
 
-  console.log(processData("abbas", { reverse: true }));
+  const ahmad: Employee = { id: 1, name: "ahmad", department: "one" };
+  const saleh: Employee = { id: 2, name: "saleh", department: "two" };
+  const hassan: Employee = { id: 3, name: "hassan", department: "three" };
+  const amir: Employee & { age: number } = {
+    id: 4,
+    name: "amir",
+    age: 21,
+    department: "four",
+  };
+
+  const abbas: Manager = {
+    id: 1,
+    name: "abbas",
+    employees: [ahmad, saleh, hassan, amir],
+  };
+
+  PrintStafDetail(abbas);
+
   return <div>App</div>;
 };
 
