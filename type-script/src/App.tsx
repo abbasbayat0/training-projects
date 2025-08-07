30b224c6e2fe33c90500a3a379760f57ad592b65
abbasbayat0 <abbas0bayat@gmail.com>
2025-08-07 11:43:47 +0330

    extended interfaces & advanced interface

--- type-script/src/App.tsx
+++ type-script/src/App.tsx
@@ -1,25 +1,39 @@
 const App = () => {
-  interface Computer {
-    readonly id: number;
-    brand: string;
-    ram: number;
-    storage?: string;
-    upgradeRam: (newRam: number) => number;
+  interface Person {
+    name: string;
+  }
+  interface DogOwner extends Person {
+    dogName: string;
+  }
+  interface Manager {
+    managePeople: () => void;
+    delegateTasks: () => void;
   }
 
-  const myComputer: Computer = {
-    id: 1,
-    brand: "asus",
-    ram: 8,
-    upgradeRam: (newRam) => {
-      return (myComputer.ram += newRam);
-    },
+  const getEmployee = (): Person | DogOwner | Manager => {
+    const randomNumber = Math.random().toFixed(2);
+    if (randomNumber < "0.33") {
+      const newPerson: Person = { name: "abbas" };
+      return newPerson;
+    } else if (randomNumber < "0.66") {
+      const newDogOwner: DogOwner = { name: "abbas", dogName: "rex" };
+      return newDogOwner;
+    }
+    const manager: Manager = {
+      managePeople: () => console.log("manage people"),
+      delegateTasks: () => console.log("delegateTasks"),
+    };
+    return manager;
   };
 
-  myComputer.storage = "512 G";
-  myComputer.upgradeRam(8);
+  const employee: Person | DogOwner | Manager = getEmployee();
+  const isManager = (obj: Person | DogOwner | Manager): obj is Manager => {
+    return "managePeople" in obj;
+  };
 
-  console.log(myComputer);
+  if (isManager(employee)) {
+    employee.delegateTasks()
+  }
   return <div>App</div>;
 };
 
