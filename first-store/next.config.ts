5cc37b87ac5101da8c57b9e3c53ac4aca3d5c4de
abbasbayat0 <abbas0bayat@gmail.com>
2025-08-17 21:09:49 +0330

    D: home footer

--- first-store/next.config.ts
+++ first-store/next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("https://images.pexels.com/photos/**")],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+    ],
   },
 };
 
--- first-store/src/components/shared/FeaturedProducts.tsx
+++ first-store/src/components/shared/FeaturedProducts.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import useFetchFeatured from "@/lib/hooks/useFetchFeatured";
 import store from "@/lib/store/store";
@@ -10,32 +11,56 @@ const FeaturedProducts = () => {
   if (status === "success") products = [...data?.data.data];
 
   return (
-    <div className="max-w-7xl flex flex-col justify-center items-center p-5">
+    <div className="max-w-7xl flex flex-col justify-center items-center p-5 mx-auto">
       <div className="flex justify-start items-start w-full">
         <h1 className={`${dark ? "text-white" : ""} text-3xl w-1/2 opacity-80`}>
           Featured Products
         </h1>
       </div>
       <div
-        className={`w-5/6 h-[1px] border mt-3 shadow bg-black mx-auto opacity-50`}
+        className={`w-full h-[1px] border mt-3 shadow bg-black mx-auto opacity-20`}
       ></div>
       <div>
         {products.length === 0 ? (
           <h1 className={`${dark && "text-white"} mt-5`}>Loading Data ...</h1>
         ) : (
-          <div className={`mt-10 flex flex-col flex-wrap gap-10 w-full p-5`}>
+          <div
+            className={`mt-10 flex flex-col flex-wrap gap-10 w-full p-5 sm:flex-row lg:gap-12`}
+          >
             {products.map(({ id, attributes }) => {
+              const formatted = (amount: number) => {
+                return new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(amount);
+              };
               return (
                 <div
                   key={id}
-                  className={`shadow-xl hover:shadow-2xl flex flex-col justify-center items-center rounded-xl mx-auto p-5`}
+                  className={`shadow-xl hover:shadow-2xl transition duration-500 flex flex-col justify-center items-center rounded-xl mx-auto p-5`}
                 >
-                  <div className="w-54 h-60 bg-amber-200 rounded-xl overflow-hidden flex justify-center items-center">
-                    <img src={attributes.image} alt={attributes.title} className="object-cover" />
+                  <div className="w-54 h-60 lg:w-60 lg:h-64 rounded-xl overflow-hidden flex justify-center items-center">
+                    <img
+                      src={attributes.image}
+                      alt={attributes.title}
+                      className="object-cover"
+                    />
                   </div>
                   <div className="flex flex-col justify-center items-center">
-                    <p className={`text-nowrap mt-2`}>{attributes.title}</p>
-                    <p className="mx-auto my-2 text-sm opacity-80">${attributes.price}</p>
+                    <p
+                      className={`text-nowrap mt-2 ${
+                        dark && "text-white"
+                      } lg:text-lg`}
+                    >
+                      {attributes.title}
+                    </p>
+                    <p
+                      className={`mx-auto my-2 text-sm opacity-80 ${
+                        dark && "text-pink-500"
+                      }`}
+                    >
+                      {formatted(Number(attributes.price))}
+                    </p>
                   </div>
                 </div>
               );
