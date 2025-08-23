0ee4556cc2edddcacc68c850b0d4a41aa9165304
abbasbayat0 <abbas0bayat@gmail.com>
2025-08-23 23:30:37 +0330

    all json file content added to model of DB

---
+++ first-store/prisma/addAllFunc.js
@@ -0,0 +1,19 @@
+const action = async () => {
+  'use server';
+  const prisma = new PrismaClient();
+  try {
+    for (const item of data) {
+      await prisma.products.create({
+        data: item,
+      });
+    }
+    await prisma.$disconnect();
+    return 'success';
+  } catch (error) {
+    console.log(error);
+    await prisma.$disconnect();
+    return 'failed';
+  }
+};
+const add = await action();
+console.log(add);
