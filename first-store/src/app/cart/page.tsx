9415c3c4e5c8f637aa621bfdc581e80330be154d
abbasbayat0 <abbas0bayat@gmail.com>
2025-08-26 16:51:41 +0330

    D: removeItem and updateAmount functionality added

--- first-store/src/app/cart/page.tsx
+++ first-store/src/app/cart/page.tsx
@@ -1,10 +1,15 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 'use client';
+import { removeItem, updateAmount } from '@/lib/features/cartSlice';
 import { RootState } from '@/lib/store/store';
 import formatted from '@/lib/utils/formatPrice';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 const page = () => {
   const dark = useSelector((state: RootState) => state.theme.dark);
   const cart = useSelector((state: RootState) => state.cartSlice.cart);
+  const dispatch = useDispatch();
+  const selectOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
   return (
     <div className={`${dark && 'bg-gray-700'} min-h-screen transition duration-500`}>
       <div className='mx-auto flex flex-col px-5 pt-16 md:flex-row md:justify-around md:pb-16'>
@@ -26,9 +31,10 @@ const page = () => {
               ></div>
             </div>
             <div>
-              {cart.map((item) => {
+              {cart.map((item, index) => {
+                const formattedPrice = formatted(Number(item.attributes?.price)).split('$');
                 return (
-                  <div key={item.id} className='mt-10'>
+                  <div key={index} className='mt-10'>
                     <img
                       src={item.attributes?.image}
                       alt={item.attributes?.title}
@@ -45,10 +51,51 @@ const page = () => {
                       >
                         {item.attributes?.company}
                       </h2>
+                      <div className='flex items-center justify-center gap-2'>
+                        <p className={`${dark && 'text-white'} opacity-80`}>color:</p>
+                        <div
+                          style={{ backgroundColor: item.attributes.color }}
+                          className={`h-4 w-4 cursor-pointer rounded-full`}
+                        ></div>
+                      </div>
+                      <div className='mt-1 flex items-center justify-center gap-2'>
+                        <p className={`${dark && 'text-white'} opacity-80`}>Amount:</p>
+                        <select
+                          defaultValue={item.attributes.amount}
+                          onChange={(e) =>
+                            dispatch(
+                              updateAmount({
+                                amount: Number(e.target.value),
+                                id: item.id,
+                                color: item.attributes.color,
+                              }),
+                            )
+                          }
+                          name='amount'
+                          id='amount'
+                          className={`w-1/2 cursor-pointer rounded-xl border opacity-80 focus:outline-none ${dark && 'bg-gray-700 text-white'}`}
+                        >
+                          {selectOptions.map((i) => {
+                            return (
+                              <option key={i} value={i}>
+                                {i}
+                              </option>
+                            );
+                          })}
+                        </select>
+                      </div>
+                      <p
+                        className='my-2 cursor-pointer text-center text-red-400'
+                        onClick={() =>
+                          dispatch(removeItem({ id: item.id, color: item.attributes.color }))
+                        }
+                      >
+                        remove
+                      </p>
                       <p
                         className={`text-center text-lg font-light opacity-80 ${dark && 'text-white'} transition duration-500`}
                       >
-                        {formatted(Number(item.attributes?.price))}
+                        $ {Number(formattedPrice[1]) * item.attributes.amount}
                       </p>
                     </div>
                   </div>
--- first-store/src/lib/features/cartSlice.ts
+++ first-store/src/lib/features/cartSlice.ts
@@ -18,14 +18,34 @@ const cartSlice = createSlice({
       );
 
       if (existingIndex >= 0) {
-        state.cart[existingIndex].attributes.amount += amount;
+        let currentAmount = state.cart[existingIndex].attributes.amount;
+        currentAmount += amount;
+        if (currentAmount > 20) currentAmount = 20;
       } else {
         state.cart.push({ id, attributes });
       }
       localStorage.setItem('cart', JSON.stringify(state.cart));
     },
+    updateAmount: (state, action: PayloadAction<{ id: number; amount: number; color: string }>) => {
+      const { id, amount, color } = action.payload;
+      const existingIndex = state.cart.findIndex(
+        (item) => item.id === id && item.attributes.color === color,
+      );
+      if (existingIndex >= 0) {
+        state.cart[existingIndex].attributes.amount = amount;
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cart));
+    },
+    removeItem: (state, action: PayloadAction<{ id: number; color: string }>) => {
+      const { id, color } = action.payload;
+      const existingIndex = state.cart.findIndex(
+        (item) => item.id === id && item.attributes.color === color,
+      );
+      state.cart.splice(existingIndex, 1);
+      localStorage.setItem('cart', JSON.stringify(state.cart));
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, updateAmount, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
